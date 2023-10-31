3b6e6289e37f0000bc1e3453a5d560d81e792b5e
Qiutong Men <qm2017@nyu.edu>
2023-10-31 10:15:20 -0400

    Update kubeadm-init.sh

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -6,12 +6,15 @@ kind: InitConfiguration
 nodeRegistration:
   kubeletExtraArgs:
     root-dir: "/k8sdata"
+  criSocket: "unix:///var/run/cri-dockerd.sock"
+---
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: ClusterConfiguration
+networking:
+  podSubnet: "10.244.0.0/16"
 EOF
 
-sudo kubeadm init \
---cri-socket unix:///var/run/cri-dockerd.sock \
---pod-network-cidr 10.244.0.0/16 \
---config kubeadm-init.yaml
+sudo kubeadm init --config kubeadm-init.yaml
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
